fda2eb4bfc193a41a61ae8db240a55b939d9f0ab
Mikhail Podshivalov <mapodshivalov@gmail.com>
2021-12-04 16:17:48 +0300

    merge branches

---
+++ app.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+use Symfony\Component\Console\Application;
+
+$app = new Application();
+$app->add(new \App\QuestionToUser());
+$app->run();
