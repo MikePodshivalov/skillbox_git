3c21292263cfcedaa002d00ac183ee2de2b04ce2
Mikhail Podshivalov <mapodshivalov@gmail.com>
2021-12-04 16:30:56 +0300

    merge branches

--- app.php
+++ app.php
@@ -1,17 +1,11 @@
 <?php
-<<<<<<< HEAD
 namespace App;
-=======
->>>>>>> repeat
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 use Symfony\Component\Console\Application;
 
 $app = new Application();
-<<<<<<< HEAD
 $app->add(new QuestionToUser());
 $app->add(new SayHello());
-=======
-$app->add(new App\Repeat());
->>>>>>> repeat
+$app->add(new Repeat());
 $app->run();
