c2052c3fcb5bfed94f9d2baec91c3e7f9e1b8d40
Mikhail Podshivalov <mapodshivalov@gmail.com>
2021-12-04 16:20:39 +0300

    merge branches

--- app.php
+++ app.php
@@ -1,8 +1,10 @@
 <?php
+namespace App;
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 use Symfony\Component\Console\Application;
 
 $app = new Application();
 $app->add(new \App\QuestionToUser());
+$app->add(new SayHello());
 $app->run();
